9660e8798a6d0d8e7d1a7adc989246ef2456cf05
Jozue06 <jozue06@gmail.com>
2018-02-20 12:04:14 -0800

    Josh added a branch and updated the cart html, index html, cart css, cart js and added some psuedo code to cart js.

---
+++ js/cart.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// local storage stringify:
+
+// var strItems = JSON.stringify(Item.all);
+// localStorage.setItem('items', strItems);
+
+// need event listener (click or submit)
+
+// need event handler (click or submit)
+
+// need local storage parse ? ?
+
+// need cart item objects, or constructor and/or array(s)
+
